8f79bd8d6b4338b1364b6ba81601052ba7ecb042
Hans Kristian Odberg Markeseth <hkmark17@student.uia.no>
2019-08-21 19:37:07 +0200

    First PHP file

--- prg1000/innlevering1/vis-alle-studenter.php
+++ prg1000/innlevering1/vis-alle-studenter.php
@@ -1,1 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Vis alle studenter</title>
+</head>
+<body>
+
 <?php
+    $file = file_get_contents("./student.txt", FILE_USE_INCLUDE_PATH);
+    var_dump($file);
+?>
+
+</body>
+</html>
+
+
+
+
