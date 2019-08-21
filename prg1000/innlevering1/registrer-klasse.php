a0427f320596751316144dbcfa3cd628203f9c35
Hans Kristian Odberg Markeseth <hkmark17@student.uia.no>
2019-08-21 22:09:30 +0200

    Added form too klassereg

--- prg1000/innlevering1/registrer-klasse.php
+++ prg1000/innlevering1/registrer-klasse.php
@@ -1,1 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+
+    </title>
+</head>
+<body>
+
+<form action="./registrer-klasse.php" method="post">
+    Klasse kode:<br>
+    <input type="text" name="klassekode"><br>
+    Klasse navn:<br>
+    <input type="text" name="klassenavn"><br>
+    <br>
+    <input type="submit" name="Submit"><br>
+
+</form>
+
+
 <?php
+    $post = $_POST;
+    var_dump($post);
+
+?>
+
+</body>
+</html>
--- prg1000/innlevering1/vis-alle-studenter.php
+++ prg1000/innlevering1/vis-alle-studenter.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <title>Vis alle studenter</title>
 </head>
@@ -8,8 +8,12 @@
 <?php
     $file = file_get_contents("./student.txt", FILE_USE_INCLUDE_PATH);
     $students = explode(";", $file);
-    for ($i = 1; $i <= 10; $i++){
+    for ($i = 1; $i <= 2; $i++){
         echo $students[$i];
+    }
+
+    foreach ($students as $name){
+
     }
 
 ?>
