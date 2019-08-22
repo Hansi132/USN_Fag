6ad7593dcb5b51cb45785d6e048601ac600d2992
Hans Kristian Odberg Markeseth <hkmark17@student.uia.no>
2019-08-22 15:37:56 +0200

    Started registrer-student

--- prg1000/innlevering1/registrer-student.php
+++ prg1000/innlevering1/registrer-student.php
@@ -1,1 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+        Registrer student
+    </title>
+</head>
+<body>
+
+<form action="./registrer-student.php" method="post">
+    Brukernavn:<br>
+    <input type="text" name="Brukernavn" required><br>
+    Fornavn:<br>
+    <input type="text" name="Fornavn" required><br>
+    Etternavn:<br>
+    <input type="text" name="Etternavn" required><br>
+    Klassekode:<br>
+    <input type="text" name="Klassekode" required><br>
+    <br>
+    <input type="submit" name=""><br>
+
+</form>
+
+
 <?php
+$post = $_POST;
+//unset($post[2]);
+$storage = implode(";", $post);
+$myfile = "./student.txt";
+$handle = fopen($myfile, 'a') or die('Cannot open file:  '.$myfile);
+$datatofile = $storage . "\n";
+fwrite($handle, $datatofile);
+fclose($handle);
+
+?>
+
+</body>
+</html>
