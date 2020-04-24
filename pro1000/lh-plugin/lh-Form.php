e9705ba58debd755586479703b58d017001b3a3d
Hans Kristian <hanskristianom@gmail.com>
2020-04-24 20:51:09 +0200

    More mail and css

--- pro1000/lh-plugin/lh-Form.php
+++ pro1000/lh-plugin/lh-Form.php
@@ -10,9 +10,9 @@
 	<label for="lh_name">Navn</label>
 	<input type="text" class="lh_name" id="lh_name" name="lh_name" value="" required>
 	<label for="lh_email">E-Post</label>
-	<input type="text" class="lh_email" id="lh_email" name="lh_email" value="" required>
+	<input type="email" class="lh_email" id="lh_email" name="lh_email" value="" required>
 	<label for="lh_telefon">Telefon</label>
-	<input type="text" class="lh_telefon" id="lh_telefon" name="lh_telefon" value="" required>
+	<input type="tel" class="lh_telefon" id="lh_telefon" name="lh_telefon" value="" required style="">
 	<label for="lh_hvorfor">Hva ønsker du?</label>
 	<textarea class="lh_hvorfor" id="lh_hvorfor" name="lh_hvorfor"></textarea>
 	<br>
